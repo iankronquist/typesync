0877e22146f7c8e740fb70c39ab14d201fbf837c
Ian Kronquist <iankronquist@teleport.com>
2015-04-19 12:56:32 -0700

    Add Hello World code

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+/// <reference path='../typings/tsd.d.ts' />
+import express = require('express');
+import bodyParser = require('body-parser');
+import knexBuilder = require('knex');
+
+var knex = knexBuilder({
+  client: process.env.CLIENT || 'sqlite3',
+  connection: process.env.DATABASE_URL || { filename: 'dev.sqlite3' }
+});
+
+var app = express();
+app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('knex', knex);
+
+//var routes = require('./routes')(app);
+
+app.get('/', function (req, res) {
+  res.send('hello typescript');
+});
+
+
+app.listen(process.env.PORT || 8000, function () {
+  console.log('App now listening on %s', process.env.Port || 8000);
+});
