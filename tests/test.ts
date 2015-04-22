d18b8be8baf0b83531fad4358b1abdcedea951c7
Ian Kronquist <iankronquist@teleport.com>
2015-04-22 14:39:49 -0700

    Add preliminary tests

---
+++ tests/test.ts
@@ -0,0 +1,16 @@
+/// <reference path='../typings/tsd.d.ts' />
+import mocha = require('mocha');
+import request_builder = require('request');
+import expect = require('expect.js');
+import app = require('../src/app');
+var request = request_builder.defaults({encoding: null});
+
+describe('GET /', function() {
+  it ('should say typescript', function(done) {
+    request.get('http://localhost:' + process.env.PORT + '/', function(err,
+        res, body) {
+      expect(res.statusCode).to.be(200);
+      expect(res.body).to.be('hello typescript');
+    });
+  });
+});
